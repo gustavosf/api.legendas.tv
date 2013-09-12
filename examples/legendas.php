385825e6cf79253f4729e2b2b5e15e7ecb9a41cf
Gustavo Seganfredo <gustavosf@gmail.com>
2013-09-12 11:11:14 -0300

    Exemplo 100% agora :)

--- examples/legendas.php
+++ examples/legendas.php
@@ -18,18 +18,31 @@ function readln()
  * l          language
  * f, first   baixa o primeiro link
  */
-$options = getopt('l:f', array('first'));
+$options = getopt('l:fd', array('first'));
 $search = implode(" ", array_slice($argv, sizeof($options) + 1));
 
 /* Começa a treta :D */
-$subtitles = LegendasTV::search($search, @$options['l']);
+$subtitles = LegendasTV::search($search, @$options['l'] ?: 'Qualquer idioma');
+
+if (array_key_exists('d', $options))
+{ # caso flag "d" esteja ativada retorna apenas os destaques 
+	$subtitles = array_filter($subtitles, function($subtitle) { return $subtitle->destaque; });
+}
+else
+{ # caso contrário, ordena por destaque e downloads
+	usort($subtitles, function($a, $b) {
+		if ($a->destaque and !$b->destaque) return -1;
+		if ($b->destaque and !$a->destaque) return 1;
+		return $a->downloads > $b->downloads ? -1 : 1;
+	});
+}
 
 if (count($subtitles) > 1 and ! (array_key_exists('f', $options) or array_key_exists('first', $options)))
 {
 	echo "Qual das legendas abaixo desejas baixar?\n\n";
 	foreach ($subtitles as $id => $subtitle)
 	{
-		echo "[{$id}] ".$subtitle->destaque ? '*' : ' '." {$subtitle->arquivo} ({$subtitle->downloads}/dl {$subtitle->idioma})\n";
+		echo sprintf("[%".(count($subtitles) > 10 ? 2 : 1)."d] %s %s (%d/dl %s)\n", $id, $subtitle->destaque ? '*' : ' ', $subtitle->arquivo, $subtitle->downloads, $subtitle->idioma);
 	}
 	$option = (int)readln();
 
