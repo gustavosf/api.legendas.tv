0873768ab0b45387af806d9436f029f0018dfb66
Gustavo Seganfredo <gustavosf@gmail.com>
2014-06-12 01:12:07 -0300

    Corrigido link para download da legenda

--- lib/legendastv.php
+++ lib/legendastv.php
@@ -163,11 +163,20 @@ class Legenda {
 	 */
 	public function download($filename = null)
 	{
-		list($file, $info, $header) = LegendasTV::request("http://legendas.tv/pages/downloadarquivo/{$this->id}");
+		// list($file, $info, $header) = LegendasTV::request("http://legendas.tv/pages/downloadarquivo/{$this->id}");
+		list($file, $info, $header) = LegendasTV::request("http://legendas.tv/downloadarquivo/{$this->id}");
 		if ($filename === null)
 		{
-			preg_match('/filename="(.*?)"/', $header, $filename);
-			$filename = $filename[1];
+			/** Antigo funcionamento. Agora não retorna mais o filename no header
+			// preg_match('/filename="(.*?)"/', $header, $filename);
+			*/
+			preg_match('/Location: http:\/\/f\.legendas\.tv\/l\/(.*)/', $header, $filename);
+
+			// O formato abaixo é o nome de retorno do arquivo do legendas.tv, que não diz muita coisa
+			$filename = trim($filename[1]);
+
+			// Alteramos para o nome de arquivo refletir o nome completo da legenda
+			$filename = $this->data['arquivo'].substr($filename, strrpos($filename, '.'));
 		}
 		file_put_contents($filename, $file);
 		return $filename;
